cb0ad608d390c9ab46b6ad8e0e4e8014d28fe203
Emah Khujaemah <khuza.emah24@gmail.com>
2021-03-14 19:50:28 +0700

    Membuat folder baru Testing dan file2 didalamnya

--- model/Mahasiswa.java
+++ model/Mahasiswa.java
@@ -19,8 +19,41 @@ public class Mahasiswa extends Person{
         System.out.println("Cetak KRS");
     }
 
-    public void setNim(String nim){      
+    public void setNim(String nim){        
+        
         this.nim = nim;
+    }
+    
+    public String setNim1(String nim){
+        boolean adaHuruf;
+        try {
+            int angka = Integer.parseInt(nim);
+            adaHuruf = false;
+        } catch (Exception e) {
+            adaHuruf = true;
+        }
+        
+        //1. Kasus ketika nim tidak boleh kosong
+        if(nim.equals("") || nim.isEmpty()){
+            return "NIM tidak boleh kosong!";   
+        }
+
+        //2. kasus ketika NIM harus 7 karakter
+        if(nim.length() != 7 ){
+            return "Panjang Karakter NIM harus 7!";
+        }
+
+        //3. Kasus ketika NIM tidak boleh huruf
+        if(adaHuruf){
+            return "NIDN harus berisi angka";
+        }
+        
+        //1. Kasus ketika inputan harus angka
+        // if (nim.matches("[0-9]*")) {
+        //     return "Inputan NIM Harus Angka";
+        // }
+
+        return this.nim = nim;
     }
 
     public String getNim(){
